964f502d1b852a105061a9e3200b31ea66b5ba61
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-11-23 00:49:44 +0100

    added front-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
