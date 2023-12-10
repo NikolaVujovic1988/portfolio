72a357122692b0e2c221c49dcff20234361ef4aa
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-12-10 13:50:51 +0100

    about me version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { FrontPageFooterComponent } from './front-page-footer/front-page-footer.component';
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FrontPageComponent,
-    FrontPageFooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FrontPageComponent,
+        FrontPageFooterComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AboutMeComponent
+    ]
 })
 export class AppModule { }
